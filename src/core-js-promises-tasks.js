2a843a5108d51e988101b0f8eec6157d3c0f8aee
Sergey <seleznev-krista@mail.ru>
2024-04-05 11:56:20 +0300

    feat: add getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -118,8 +118,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getAllResult(promises) {
+  const result = [];
+  const pr = await Promise.allSettled(promises);
+  pr.forEach((elem) => {
+    if (elem.status === 'fulfilled') result.push(elem.value);
+    else result.push(null);
+  });
+  return result;
 }
 
 /**
